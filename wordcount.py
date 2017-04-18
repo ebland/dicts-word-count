7787ca5094587ef59c2663b438dea4ffb1805ac4
E <gencomsolar@users.noreply.github.com>
2017-04-17 18:29:54 -0700

    Update wordcount.py

--- wordcount.py
+++ wordcount.py
@@ -25,3 +25,34 @@ def get_count_for_word(word_counts):
 
 word_tally = get_word_counts("test.txt")
 get_count_for_word(word_tally)
+
+
+##############################################################################################################
+
+import sys
+import string
+import collections
+
+def count_words(my_file):
+    text_file = open(my_file)
+    text = text_file.read()
+    words = text.split(" ")
+    for word in words:
+        word = word.lower()
+        word = word.strip(string.whitespace)
+        word = word.rstrip(string.punctuation)
+    c = collections.Counter(words)
+    for word, count in c.items():
+        print word, count
+    # word_count = {}
+    # for line in text:
+    #     line = line.rstrip()
+    #     words = line.split(" ")
+    #     for word in words:
+    #         word = word.lower()
+    #         word = word.strip(string.punctuation)
+    #         word_count[word] = word_count.get(word, 0) + 1
+    # for word, count in word_count.items():
+    #     print word, count
+    text_file.close()
+count_words(my_file = sys.argv[-1])
